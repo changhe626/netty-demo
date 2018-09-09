526ae8a4e5669571939e2c8933f5eea464190ac0
zhangke <changhe626@163.com>
2018-09-09 11:35:38 +0800

    添加了源码 2018年9月9日11:35:34

--- src/main/java/com/onyx/netty/Start.java
+++ src/main/java/com/onyx/netty/Start.java
@@ -1,26 +1,20 @@
 package com.onyx.netty;
 
 import com.onyx.netty.pool.NioSelectorRunnablePool;
-import java.net.InetSocketAddress;
 import java.util.concurrent.Executors;
 
 /**
  * 启动主函数
- * 从 netty 中抽取的功能...
  */
 public class Start {
 
 	public static void main(String[] args) {
 		//初始化线程
 		NioSelectorRunnablePool nioSelectorRunnablePool = new NioSelectorRunnablePool(Executors.newCachedThreadPool(), Executors.newCachedThreadPool());
-		
 		//获取服务类
 		ServerBootstrap bootstrap = new ServerBootstrap(nioSelectorRunnablePool);
-		
 		//绑定端口
 		bootstrap.bind(10010);
-		
 		System.out.println("start");
 	}
-
 }
